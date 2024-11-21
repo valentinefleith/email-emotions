171c159b06cd0f170b44cec677b95e3f038754ad
valentinefleith <valentine.fleith@gmail.com>
2024-11-21 18:54:49 +0100

    Add stats

--- src/classify.py
+++ src/classify.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from collections import Counter
 from Classifier import Classifier
 
 PATH = "../data_featurized.csv"
@@ -13,15 +14,31 @@ def get_data_arr(df):
     return X, y
 
 
+def get_stats(y):
+    index_class_counts = Counter(y)
+    for i, name in enumerate(LABEL_LIST):
+        print(index_class_counts[i])
+    str_class_counts = {
+        name: index_class_counts[i] for i, name in enumerate(LABEL_LIST)
+    }
+    stats_df = pd.DataFrame(list(str_class_counts.items()), columns=["Class", "Count"])
+    stats_df["Percentage"] = stats_df["Count"] / stats_df["Count"].sum() * 100
+    print(stats_df)
+
+
+def classify(type, X, y):
+    classifier = Classifier(type, X, y, LABEL_LIST)
+    y_hat = classifier.classify(best_params=None)
+    classifier.get_classificationReport(y_hat, save=True)
+    classifier.get_and_plot_confusionMatrix(y_hat, save=True)
+
+
 def main():
     df = pd.read_csv(PATH)
     X, y = get_data_arr(df)
-    print(X.shape)
-    print(y.shape)
-    svm_classifier = Classifier("SVM", X, y, LABEL_LIST)
-    y_hat = svm_classifier.classify(best_params=None)
-    svm_classifier.get_classificationReport(y_hat, save=True)
-    svm_classifier.get_and_plot_confusionMatrix(y_hat, save=True)
+    get_stats(y)
+    # classify("SVM", X, y)
+    classify("RFC", X, y)
 
 
 main()
